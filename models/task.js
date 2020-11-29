4b430876821c45bf8a1b86be816de78b56968205
diannamcallister <dd.mcallister@hotmail.ca>
2020-11-29 13:03:30 -0500

    began adding mongoose stuff

---
+++ models/task.js
@@ -0,0 +1,67 @@
+/* Student mongoose model */
+const mongoose = require('mongoose')
+const UserSchema  = require("./user").schema;
+
+const CommentSchema = new mongoose.Schema({
+    commenter: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'user'
+	},
+	comment: String
+});
+
+const Task = mongoose.model('Task', {
+	owner: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'user' 
+	},
+	image: {
+		data: Buffer, 
+		contentType: String
+	},
+	location: {
+		type: String,
+		required: true,
+		minlegth: 1,
+		trim: true
+	},
+	title: {
+		type: String,
+		required: true,
+		minlegth: 1,
+		trim: true
+	},
+	description: {
+		type: String,
+		required: true,
+		minlegth: 1,
+		trim: true
+	},
+	numVolunteers: {
+		type: Number,
+		required: true,
+		minlegth: 1,
+		trim: true
+	},
+	numHours: {
+		type: Number,
+		required: true,
+		minlegth: 1,
+		trim: true
+	},
+	price: {
+		type: Number,
+		required: true,
+		minlegth: 1,
+		trim: true
+	},
+	isReported: {
+		type: Boolean,
+		required: true,
+		minlegth: 1,
+		trim: true
+	},
+	comments: [CommentSchema]
+})
+
+module.exports = { Task }
